4845b2b70d32b02f88032748581caea387b12867
Maksim Saybotalov <93943808+tepuk@users.noreply.github.com>
2022-10-29 10:02:52 +0700

    add doc

--- lab4/TextAnalyzer.java
+++ lab4/TextAnalyzer.java
@@ -1,3 +1,12 @@
-public interface TextAnalyzer {
+/**
+ * Производит анализ строки по интересующему критерию
+ */
+public interface StringAnalyzer {
+    /**
+     * Анализ строки по интересующему критерию
+     *
+     * @param str исходная строка
+     * @return результат анализа
+     */
     int analyse(String str);
 }
